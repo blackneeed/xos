a1b768b05b4c96854bcc7a38c529a731abe498d3
Blackneeed <kontododatkowe.marcelclay@gmail.com>
2024-08-12 21:22:09 +0200

    Clearing the screen

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-[org 0x7c00] ; say all memory addresses have to be offsetted by 0x7c00 (where the bios loads us)
+[org 7c00h] ; say all memory addresses have to be offsetted by 0x7c00 (where the bios loads us)
 [bits 16]
 
 main:
@@ -8,9 +8,23 @@ main:
 
     jmp 0:.after_far_jump
 .after_far_jump:
+    ; setting up the stack
     mov ss, ax
-    mov bp, 0x7c00
-    mov sp, 0x7c00
+    mov bp, 7c00h
+    mov sp, 7c00h
+
+    ; clearing the screen
+    ; ah=07h int 10h -- BUGS: Some implementations (including the original IBM PC) have a bug which destroys BP. The Trident TVGA8900CL (BIOS dated 1992/9/8) clears DS to 0000h when scrolling in an SVGA mode (800x600 or higher)
+    ; http://www.ctyme.com/intr/rb-0097.htm
+    push bp
+    mov ah, 07h
+    mov al, 00h
+    mov bh, 0fh
+    xor cx, cx
+    mov dl, 80
+    mov dh, 25
+    int 10h
+    pop bp
 
     jmp $
 
